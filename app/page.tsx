210b051bc959c8583e2f36903c388e479d018eae
saipepu <saipepu.mdy257@gmail.com>
2023-11-03 21:28:07 +0700

    2nd day

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@/components/ui/button'
-
-export default function Home() {
-  return (
-    <div>
-      <Button variant="secondary">Click Me</Button>
-    </div>
-  )
-}
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://sought-chimp-78.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
